fba992af8f3e60c6e36882f4f01a7dc6966c2866
KBA-Aslam <kbasayemaslam@gmail.com>
2020-08-19 00:16:17 +0600

    All credit goes to shrikanta vai and kumu apu

--- lyric.js
+++ lyric.js
@@ -9,27 +9,26 @@ searchBox.addEventListener("click", function(){
     .then(data => {
         let lyricList = document.getElementById('titleOne');
         lyricList.innerHTML = "";
-
-        // console.log(data);
+        console.log(data);
         for (let i = 0; i < 10; i++) {
-            const t  = data.data[i];
-            let artist = t.artist;
-            let album = t.album;
+
+            const element = data.data[i];
+            const songTitle = element.title;
+            const artistName = element.artist.name;
+
 
             lyricList.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
             <div class="col-md-9">
-                <h3 class="lyrics-name"> <strong> ${album.title} </strong> </h3>
-                <p class="author lead"> Album by <span> ${artist.name} </span></p>
+                <h3 class="lyrics-name"> <strong> ${songTitle} </strong> </h3>
+                <p class="author lead"> Album by <span> ${artistName} </span></p>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="loadLyrics('${artistName}', '${songTitle}')" class="btn btn-success">Get Lyrics</button>
             </div>
         </div>`
 
-            let artistName = artist.name;
-            let title= album.title;
-
-            loadLyrics(artistName, title);    
+            
+                
         }
 })
 })
@@ -39,32 +38,38 @@ const lyricsContainer = document.getElementById("lyrics");
 const lyricsTitle = document.querySelector("#single-lyrics h2");
 
 function loadLyrics(artistName, title){
-        const getLyric = document.getElementById('getLyric');
-
-        getLyric.addEventListener("click", function() {
+        console.log(artistName, title);
+        lyricsContainer.innerHTML = "";
             fetch(`https://api.lyrics.ovh/v1/${artistName}/${title}`)
             .then(res => res.json())
             .then(data => {
-                return data;
-        })
-    })
-}
-function getLyrics(artistName, title) {
-    toggleElement(lyricList, singleLyrics);
-    const lyrics = loadLyrics(artistName, title);
+                
+                if (data.lyrics) {
+                    console.log(data.lyrics)
+                                lyricsContainer.innerHTML += data.lyrics;
+                            } else {
+                                lyricsContainer.innerHTML += "Sob lyric dekhate hobe emon kono kotha nai! Amar mon chaile dekhabo na!";
+                            }
+                            lyricsTitle.innerHTML += title + " - " + artistName;
+                        });
+                    }
 
-    lyrics.then((lyric) => {
-        if (lyric.lyrics) {
-            lyricsContainer.innerHTML = lyric.lyrics;
-        } else {
-            lyricsContainer.innerHTML = "Sorry! Lyrics not available.";
-        }
-        lyricsTitle.innerHTML = title + " - " + artistName;
-    });
-}
+// function getLyrics(artistName, title) {
+//     toggleElement(lyricList, singleLyrics);
+//     const lyrics = loadLyrics(artistName, title);
 
-//change Element display state
-function toggleElement(hideElement, displayElement) {
-    hideElement.style.display = "none";
-    displayElement.style.display = "block";
-}
+//     lyrics.then((lyric) => {
+//         if (lyric.lyrics) {
+//             lyricsContainer.innerHTML = lyric.lyrics;
+//         } else {
+//             lyricsContainer.innerHTML = "Sob lyric dekhate hobe emon kono kotha nai!";
+//         }
+//         lyricsTitle.innerHTML = title + " - " + artistName;
+//     });
+// }
+
+// //change Element display state
+// function toggleElement(hideElement, displayElement) {
+//     hideElement.style.display = "none";
+//     displayElement.style.display = "block";
+// }
